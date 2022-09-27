c234e1745c3399a2e680b265bc80ff0ae0018ddd
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-27 13:05:01 +0300

    Add seed file for greeting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: 'Hola')
+Greeting.create(message: 'Merhaba')
+Greeting.create(message: 'Hello')
+Greeting.create(message: 'Hallo')
+Greeting.create(message: 'Bonjour')
